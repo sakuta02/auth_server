ff7db15d03ddf156055af3215bba929fffc1a376
savam <savam.0510@gmail.com>
2024-08-07 17:46:48 +0500

    1.1

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,12 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from authentication.models import Device
+from authentication.models import Device, Status
 from datetime import datetime
 from authentication.serializers import DeviceSerializer, HandleDeviceSerializer
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import decorators, status
 
 
 class DeviceAuthView(ModelViewSet):
@@ -18,8 +20,16 @@ class DeviceAuthView(ModelViewSet):
         return super().retrieve(request, *args, **kwargs)
 
 
-class HandleAuthView(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin):
+class HandleAuthView(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     serializer_class = HandleDeviceSerializer
     queryset = Device.objects.all()
+
+    @decorators.action(methods=['delete'], detail=False)
+    def clear(self, request):
+        devices = list(Device.objects.filter(status=Status.NOT_VIEWED.value))
+        count_devices = len(devices)
+        for device in devices:
+            device.delete()
+        return Response(data={'count_devices': count_devices}, status=status.HTTP_204_NO_CONTENT)
